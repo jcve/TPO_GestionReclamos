8e4a94341ef795da33e25edcef1751b3088188d4
jcve <jcvonelm@gmail.com>
2020-09-04 19:30:53 -0300

    Swagger & MySQL

--- GestionReclamos/Startup.cs
+++ GestionReclamos/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySqlConnector;
 
 namespace GestionReclamos
 {
@@ -28,6 +29,10 @@ namespace GestionReclamos
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddTransient<MySqlConnection>(_ => new MySqlConnection(Configuration["ConnectionStrings:Database"]));
+            services.AddSwaggerGen();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +52,12 @@ namespace GestionReclamos
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestion Reclamos API");
+            });
 
             app.UseRouting();
 
