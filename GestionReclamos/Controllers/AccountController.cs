cef276abd33f20a2d64918e236f8719f6009d2bd
jcve <jcvonelm@gmail.com>
2020-09-25 00:09:12 -0300

    Limpieza de using no usados

--- GestionReclamos/Controllers/AccountController.cs
+++ GestionReclamos/Controllers/AccountController.cs
@@ -4,12 +4,9 @@ using GestionReclamos.Models.Response;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace GestionReclamos.Controllers
 {
